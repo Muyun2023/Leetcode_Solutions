209716d363e0940aba26e34f7dfdec86de227797
Muyun2023 <123432947+Muyun2023@users.noreply.github.com>
2024-12-13 21:06:41 -0800

    Update 0063-unique-paths-ii.go

--- go/0063-unique-paths-ii.go
+++ go/0063-unique-paths-ii.go
@@ -12,16 +12,6 @@ func uniquePathsWithObstacles(obstacleGrid [][]int) int {
     }
     
     dp[0][1] = 1
-    
-    for i := 1; i <= m; i++ {
-        for j := 1; j <= n; j++ {
-            if obstacleGrid[i-1][j-1] == 1 {
-                dp[i][j] = 0
-            } else {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
-            }
-        }
-    }
 
     return dp[m][n];
 }
