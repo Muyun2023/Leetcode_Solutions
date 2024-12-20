49cb7cddfd62af9001a834b958ad6a6abe5c6d5f
Muyun2023 <123432947+Muyun2023@users.noreply.github.com>
2024-12-19 17:54:33 -0800

    Update 0417-pacific-atlantic-water-flow.go

--- go/0417-pacific-atlantic-water-flow.go
+++ go/0417-pacific-atlantic-water-flow.go
@@ -19,24 +19,4 @@ func pacificAtlantic(heights [][]int) [][]int {
         dfs(r, c + 1, visit, heights[r][c])
         dfs(r, c - 1, visit, heights[r][c])
     }
-    
-    for c := 0; c < COLS; c++ {
-        dfs(0, c, pac, heights[0][c])
-        dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
-    }
-    
-    for r := 0; r < ROWS; r++ {
-        dfs(r, 0, pac, heights[r][0])
-        dfs(r, COLS - 1, atl, heights[r][COLS - 1])
-    }
-    
-    res := make([][]int, 0)
-    for r := 0; r < ROWS; r++ {
-        for c := 0; c < COLS; c++ {
-            if pac[r*COLS + c] && atl[r*COLS + c] {
-                res = append(res, []int{r, c})
-            }
-        }
-    }
-    return res
 }
