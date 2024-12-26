cc7752102c2c04a37fbbcc8c07ed8bf2f423b8e7
Muyun2023 <123432947+Muyun2023@users.noreply.github.com>
2024-12-25 16:53:18 -0800

    Update 1669-merge-in-between-linked-lists.go

--- go/1669-merge-in-between-linked-lists.go
+++ go/1669-merge-in-between-linked-lists.go
@@ -1,10 +1,3 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func mergeInBetween(list1 *ListNode, a int, b int, list2 *ListNode) *ListNode {
 	curr := list1
 	i := 0
