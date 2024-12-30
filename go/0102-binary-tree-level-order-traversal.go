03b5653ebd5477e76467398c83a2a1d834dbb62b
Muyun2023 <123432947+Muyun2023@users.noreply.github.com>
2024-12-29 18:59:42 -0800

    Update 0102-binary-tree-level-order-traversal.go

--- go/0102-binary-tree-level-order-traversal.go
+++ go/0102-binary-tree-level-order-traversal.go
@@ -16,24 +16,18 @@ func levelOrder(root *TreeNode) [][]int {
 				q = q[1:]
 				
 				if len(result) <= level {
-				 //create new slice with first value of the level and append to result matrix
 					result = append(result,[]int{node.Val})
 				}else {
-				//add values to the slice created per level
 					result[level] = append(result[level],node.Val)
 				}
 				
-				//queue implementation : add element at Back( after last element of slice by append)
-				//add left node at queue
 				if node.Left != nil {
 					q = append(q,node.Left)
 				}
-				//add Right node at queue
 				if node.Right != nil{
 					q = append(q,node.Right)
 				}
 			}
-			//increment the level after adding current elements to result
 			level++
 		}
 		return result
