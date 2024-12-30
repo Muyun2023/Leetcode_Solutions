53b2d99de7f6bddc72929405aa63812a4b641459
Muyun2023 <123432947+Muyun2023@users.noreply.github.com>
2024-12-29 19:00:29 -0800

    Update 0112-path-sum.go

--- go/0112-path-sum.go
+++ go/0112-path-sum.go
@@ -1,12 +1,3 @@
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
-
 func hasPathSum(root *TreeNode, targetSum int) bool {
 	if root == nil {
 		return false
