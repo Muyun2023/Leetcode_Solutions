d119ae702afac86588a7bbef9e5d3fc2f9f9d2b5
Muyun2023 <123432947+Muyun2023@users.noreply.github.com>
2024-12-31 14:27:48 -0800

    Update 0041-first-missing-positive.go

--- go/0041-first-missing-positive.go
+++ go/0041-first-missing-positive.go
@@ -1,10 +1,8 @@
 func firstMissingPositive(nums []int) int {
 	for i := range nums {
 		for nums[i] != i+1 && (nums[i] > 0 && nums[i] <= len(nums)) && nums[i] != nums[nums[i]-1] {
-			// Swap nums[i] with the number at its correct position (nums[nums[i]-1])
 			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
 		}
 	}
-	// If no missing positive number was found, return the length of the array + 1
-	return len(nums) + 1
+、	return len(nums) + 1
 }
