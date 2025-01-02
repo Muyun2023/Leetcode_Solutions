1fd34b2d3d8ee449ef3bb0ab7c29091d3ec74c0d
Muyun2023 <123432947+Muyun2023@users.noreply.github.com>
2025-01-02 12:44:42 -0800

    Update 0019-remove-nth-node-from-end-of-list.go

--- go/0019-remove-nth-node-from-end-of-list.go
+++ go/0019-remove-nth-node-from-end-of-list.go
@@ -1,10 +1,4 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
     if head == nil {
         return head
