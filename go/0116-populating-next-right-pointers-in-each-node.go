016a84963ca2ec8301fd724b8beded3a3611f696
Muyun2023 <123432947+Muyun2023@users.noreply.github.com>
2025-01-10 17:24:51 -0800

    Update 0116-populating-next-right-pointers-in-each-node.go

--- go/0116-populating-next-right-pointers-in-each-node.go
+++ go/0116-populating-next-right-pointers-in-each-node.go
@@ -1,13 +1,3 @@
-/**
- * Definition for a Node.
- * type Node struct {
- *     Val int
- *     Left *Node
- *     Right *Node
- *     Next *Node
- * }
- */
-
 func connect(root *Node) *Node {
 	populate(root)
 	return root
