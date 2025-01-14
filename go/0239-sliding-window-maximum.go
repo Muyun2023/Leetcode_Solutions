26603938579c4d2f99ec8cfefc7c4c9d1e434e58
Muyun2023 <123432947+Muyun2023@users.noreply.github.com>
2025-01-14 10:30:48 -0800

    Update 0239-sliding-window-maximum.go

--- go/0239-sliding-window-maximum.go
+++ go/0239-sliding-window-maximum.go
@@ -4,13 +4,10 @@ func maxSlidingWindow(nums []int, k int) []int {
 	l, r := 0, 0
 
 	for r < len(nums) {
-		// pop smaller values from q
 		for len(q) != 0 && nums[q[len(q)-1]] < nums[r] {
 			q = q[:len(q)-1]
 		}
 		q = append(q, r)
-
-		// remove left val from window
 		if l > q[0] {
 			q = q[1:]
 		}
