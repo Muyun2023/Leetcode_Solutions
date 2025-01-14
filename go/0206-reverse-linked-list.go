cdaa45444a913568e552c3ae77a110a01d047603
Muyun2023 <123432947+Muyun2023@users.noreply.github.com>
2025-01-14 10:29:37 -0800

    Update 0206-reverse-linked-list.go

--- go/0206-reverse-linked-list.go
+++ go/0206-reverse-linked-list.go
@@ -1,10 +1,3 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func reverseList(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
