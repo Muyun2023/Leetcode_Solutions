3be1fb6315a66f42c1fa88fa24634240c91691c3
Muyun <123432947+Muyun2023@users.noreply.github.com>
2025-01-31 17:29:02 -0800

    Update 0669-trim-a-binary-search-tree.go

--- go/0669-trim-a-binary-search-tree.go
+++ go/0669-trim-a-binary-search-tree.go
@@ -1,11 +1,3 @@
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
 func trimBST(root *TreeNode, low int, high int) *TreeNode {
 	if root == nil {
 		return nil
