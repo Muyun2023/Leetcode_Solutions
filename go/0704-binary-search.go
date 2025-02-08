423e0fac86d68fea0a102a21a506d429bc2716cf
Muyun <123432947+Muyun2023@users.noreply.github.com>
2025-02-07 16:36:38 -0800

    Update 0704-binary-search.go

--- go/0704-binary-search.go
+++ go/0704-binary-search.go
@@ -1,16 +1,12 @@
-package main
-
 func search(nums []int, target int) int {
 	left := 0
-	right := len(nums) - 1
-
+	right := len(nums) 
 	for left <= right {
 		middle := (left + right) / 2
-
 		if nums[middle] == target {
 			return middle
 		} else if nums[middle] < target {
-			left = middle + 1
+			left = middle
 		} else {
 			right = middle - 1
 		}
