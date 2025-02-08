376d6a578acc24ca1b28d714477d525bccf2f57a
Muyun <123432947+Muyun2023@users.noreply.github.com>
2025-02-07 16:33:32 -0800

    Update 0252-meeting-rooms.go

--- go/0252-meeting-rooms.go
+++ go/0252-meeting-rooms.go
@@ -1,17 +1,3 @@
-/**
- * Definition of Interval:
- * type Interval struct {
- *     Start, End int
- * }
- */
-
-/**
- * @param intervals: an array of meeting time intervals
- * @return: if a person could attend all meetings
- */
-
-import "sort"
-
 func CanAttendMeetings(intervals []*Interval) bool {
 	// Write your code here
 	sort.Slice(intervals, func(i, j int) bool {
