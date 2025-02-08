0d58a461c5600ff065bedd270e3aa44b8d617d17
Muyun <123432947+Muyun2023@users.noreply.github.com>
2025-02-07 16:32:52 -0800

    Update 0202-happy-number.go

--- go/0202-happy-number.go
+++ go/0202-happy-number.go
@@ -1,23 +1,13 @@
-func isHappy(n int) bool {
-    
+func isHappy(n int) bool {    
 	total := 0
 	alreadySeen := make(map[int]bool)
 	for {
-
-		// if we have seen this number
 		if seen := alreadySeen[n]; seen {
 			break
 		}
-
 		alreadySeen[n] = true
-
-		// split n into its individual digits.
 		strn := fmt.Sprint(n)
-
-		// get the length
 		length := len(strn)
-
-		// compute sum of digits
 		for i := 0; i < length; i++ {
 			digit, _ := strconv.Atoi(string(strn[i]))
 			total += (digit * digit)
@@ -25,11 +15,8 @@ func isHappy(n int) bool {
 		if total == 1 {
 			return true
 		}
-
-		// reassign n to total
 		n = total
 		total = 0
-   }
-    
+   }   
 return false
 }
