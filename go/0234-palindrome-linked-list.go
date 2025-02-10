4ec35b1c5180362888a668eb1623b78705e39b6d
Muyun <123432947+Muyun2023@users.noreply.github.com>
2025-02-09 21:33:11 -0800

    Update 0234-palindrome-linked-list.go

--- go/0234-palindrome-linked-list.go
+++ go/0234-palindrome-linked-list.go
@@ -1,7 +1,6 @@
 func isPalindrome(head *ListNode) bool {
 	slow, fast := head, head
 	var rev *ListNode
-
 	for fast != nil && fast.Next != nil {
 		fast = fast.Next.Next
 		next_slow := slow.Next
@@ -9,15 +8,9 @@ func isPalindrome(head *ListNode) bool {
 		rev = slow
 		slow = next_slow
 	}
-
-	// if fast is not null, slow is middle  of
-	// odd length list which is skipped
-	// if fast is null, slow is first element of
-	// the 2nd half of even length list
 	if fast != nil {
 		slow = slow.Next
 	}
-
 	for slow != nil {
 		if slow.Val != rev.Val {
 			return false
@@ -25,6 +18,5 @@ func isPalindrome(head *ListNode) bool {
 		slow = slow.Next
 		rev = rev.Next
 	}
-
 	return true
 }
