28699ba4dc1e44ab0ae9f9295857bea2ef8cba91
Muyun <123432947+Muyun2023@users.noreply.github.com>
2025-02-09 21:34:54 -0800

    Update 0622-design-circular-queue.go

--- go/0622-design-circular-queue.go
+++ go/0622-design-circular-queue.go
@@ -67,14 +67,3 @@ func (this *MyCircularQueue) IsEmpty() bool {
 func (this *MyCircularQueue) IsFull() bool {
 	return this.length == this.capacity
 }
-
-/**
- * Your MyCircularQueue object will be instantiated and called as such:
- * obj := Constructor(k);
- * param_1 := obj.EnQueue(value);
- * param_2 := obj.DeQueue();
- * param_3 := obj.Front();
- * param_4 := obj.Rear();
- * param_5 := obj.IsEmpty();
- * param_6 := obj.IsFull();
- */
