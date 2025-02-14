a1c007d8ae0683dae06ca4670b28e8549c7355f3
Muyun <123432947+Muyun2023@users.noreply.github.com>
2025-02-14 13:42:29 -0800

    Update 0054-spiral-matrix.go

--- go/0054-spiral-matrix.go
+++ go/0054-spiral-matrix.go
@@ -1,5 +1,4 @@
 var visited = 150 // -100 <= matrix[i][j] <= 100
-
 func spiralOrder(matrix [][]int) []int {
 	n, m := len(matrix[0]), len(matrix)
 	x, y, dx, dy := 0, 0, 1, 0
@@ -11,7 +10,6 @@ func spiralOrder(matrix [][]int) []int {
 		ans = append(ans, matrix[y][x])
 		matrix[y][x] = visited
 		x, y = x+dx, y+dy
-	}
-	
+	}	
 	return ans
 }
