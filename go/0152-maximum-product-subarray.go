d972d2c9c116ad78c788093dbca0a380bc595f64
Muyun <123432947+Muyun2023@users.noreply.github.com>
2025-02-20 14:31:27 -0800

    Update 0152-maximum-product-subarray.go

--- go/0152-maximum-product-subarray.go
+++ go/0152-maximum-product-subarray.go
@@ -1,16 +1,5 @@
-func maxProduct(nums []int) int {
-    res, curMin, curMax := nums[0], 1, 1
-    
-    for i := 0; i < len(nums); i++ {
-        temp := curMax * nums[i]
-        curMax = max(max(nums[i] * curMax, nums[i] * curMin), nums[i])
-        curMin = min(min(temp, nums[i] * curMin), nums[i])
-        res = max(res, curMax)
-    }
-    return curMin
-}
-
 func max(a int, b int) int {
+    res, curMin, curMax := nums[0], 1, 1
     if (a > b) {
         return a
     }
