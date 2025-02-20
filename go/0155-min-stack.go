3de0609523739043ab06f20cd4f9e2b67474cfcb
Muyun <123432947+Muyun2023@users.noreply.github.com>
2025-02-20 14:32:18 -0800

    Update 0155-min-stack.go

--- go/0155-min-stack.go
+++ go/0155-min-stack.go
@@ -37,12 +37,3 @@ func (this *MinStack) GetMin() int {
     return this.min; 
 }
 
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Push(val);
- * obj.Pop();
- * param_3 := obj.Top();
- * param_4 := obj.GetMin();
- */
