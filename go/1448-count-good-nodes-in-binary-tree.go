18a455f010378029fdd668945cf869627fba6aa6
Muyun Ji <123432947+Muyun2023@users.noreply.github.com>
2025-03-13 22:04:24 -0700

    Update 1448-count-good-nodes-in-binary-tree.go

--- go/1448-count-good-nodes-in-binary-tree.go
+++ go/1448-count-good-nodes-in-binary-tree.go
@@ -1,23 +1,17 @@
-
  func goodNodes(root *TreeNode) int {
     return goodNodesUtil(root, root.Val)
 }
-
 func goodNodesUtil(root *TreeNode, parent int) int {
     if root == nil {
         return 0
     }
-    
     res := 1
     max := root.Val
-    
     if parent > root.Val {
         res = 0
         max = parent
     }
-    
     res += goodNodesUtil(root.Left, max)
     res += goodNodesUtil(root.Right, max)
-    
     return res
 }
