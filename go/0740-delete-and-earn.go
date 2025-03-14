58211a9ded5fff8942638671a67090a2f42a19c7
Muyun Ji <123432947+Muyun2023@users.noreply.github.com>
2025-03-13 22:03:14 -0700

    Update 0740-delete-and-earn.go

--- go/0740-delete-and-earn.go
+++ go/0740-delete-and-earn.go
@@ -1,8 +1,6 @@
 func deleteAndEarn(nums []int) int {
 	count := make(map[int]int)
-
 	unique := make([]int, 0)
-
 	for _, num := range nums {
 		if _, ok := count[num]; !ok {
 			unique = append(unique, num)
@@ -10,11 +8,7 @@ func deleteAndEarn(nums []int) int {
 
 		count[num]++
 	}
-
-	sort.Ints(unique)
-
 	earn1, earn2 := 0, 0
-
 	for i := 0; i < len(unique); i++ {
 		currEarn := unique[i] * count[unique[i]]
 
@@ -28,10 +22,8 @@ func deleteAndEarn(nums []int) int {
 			earn1 = temp
 		}
 	}
-
 	return earn2
 }
-
 func max(a, b int) int {
 	if a > b {
 		return a
